e639b1cb9dd59f294cebe4b92270cab51dbd6e5f
SreelakshmiRK <82243384+SreelakshmiRK@users.noreply.github.com>
2021-04-13 14:16:17 -0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+eats = {'APPLE':'FRUIT', 'POTATO':'ROOT', 'OKRA':'VEGETABLE'}
+
+
+# Change a Keys value
+print("-------------------------------Dictionary eats-------------------------")	
+print(eats)
+del eats['OKRA']
+print("-------------Dictionary eats after deleting the key OKRA---------------")
+print(eats)
